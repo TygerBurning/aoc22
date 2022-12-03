eb82df3f8b76886b93a2e3c81344801f1ca42873
Andrew Wright <andrew.wright@metaswitch.com>
2022-12-03 19:55:16 +0000

    day 01

--- src/day01.rs
+++ src/day01.rs
@@ -1,26 +1,32 @@
-use itertools::izip;
-
-fn count_increases(readings: &[usize]) -> usize {
-    readings
-        .iter()
-        .zip(readings[1..].iter())
-        .filter(|(prev, next)| prev < next)
-        .count()
-}
-
 pub fn day01() {
-    let input = std::fs::read_to_string("inputs/day01.txt").unwrap();
-    let readings: Vec<usize> = input.lines().map(|line| line.parse().unwrap()).collect();
+    let readings = include_str!("../inputs/day01.txt");
+    let groups = readings.split("\n\n").map(|elf_group|
+        elf_group.lines().map(|l| l.parse::<u32>().unwrap()).sum::<u32>()
+    );
 
-    println!("Part A answer is: {}", count_increases(&readings));
+    let top_3 = groups.fold([0,0,0], |mut acc, elem| {
+        // lol. Kinda cute though.
+        if elem > acc[0] {
+            if acc[0] > acc[1] {
+                if acc[1] > acc[2] {
+                    acc[2] = acc[1];
+                }
+                acc[1] = acc[0]
+            }
+            acc[0] = elem;
+        }
+        else if elem > acc[1] {
+            if acc[1] > acc[2] {
+                acc[2] = acc[1];
+            }
+            acc[1] = elem;
+        }
+        else if elem > acc[2] {
+            acc[2] = elem;
+        }
+        acc
+    });
 
-    let x: Vec<usize> = izip!(
-        &readings,
-        &readings[1..],
-        &readings[2..]
-    )
-    .map(|(x, y, z)| x + y + z)
-    .collect();
-
-    println!("Part B answer is: {}", count_increases(&x))
+    println!("Part A answer is: {:?}", top_3.iter().max().unwrap());
+    println!("Part B answer is: {:?}", top_3.iter().sum::<u32>());
 }
